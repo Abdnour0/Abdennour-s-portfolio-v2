57ef9442f2a82b619d978c8af25cecbc70c5fe40
Abdnour0 <abdnourguellaa50@gmail.com>
2026-03-31 23:19:54 +0100

    fix: mobile nav, availability logic, cursor glow, page refresh, hero scroll, get in touch link

--- js/script.js
+++ js/script.js
@@ -28,8 +28,8 @@ const lenis = new Lenis({
   gestureOrientation: 'vertical',
   smoothWheel: true,
   wheelMultiplier: 1,
-  smoothTouch: true,
-  touchMultiplier: 1.5,
+  smoothTouch: false,
+  touchMultiplier: 2,
   infinite: false,
 });
 
